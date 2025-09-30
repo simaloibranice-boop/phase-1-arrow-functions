9e10f7a863734738df4bae73d6bf9bac5a34e7ee
Branice Simaloi <simaloibranice@gmail.com>
2025-09-30 21:18:18 +0300

    Complete arrow functions lab with divide, square, add

---
+++ index.js
@@ -0,0 +1,10 @@
+// 1. Function expression called divide
+const divide = function() {
+  return 2000 / 100;
+};
+
+// 2. Arrow function called square
+const square = x => x * x;
+
+// 3. Arrow function called add
+const add = (a, b) => a + b;
